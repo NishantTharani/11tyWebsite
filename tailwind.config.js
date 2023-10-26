fc9676cff7c5c8290bbf5677d5eab16635757085
Nishant Tharani <nishant.tharani.work@gmail.com>
2023-10-26 19:56:09 +0000

    Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./_site/**/*.{html,njk}"],
+
+  darkMode: "class",
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "body-bg-light": "#EAEDF0",
+        "secondary-bg-light": "#64ffda1a",
+        "text-light": "#343A40",
+        "text-secondary-light": "#6C757D",
+        "secondary-light": "#F8F9FA",
+
+        "body-bg-dark": "#18191A",
+        "secondary-bg-dark": "#212529",
+        "text-dark": "#E4E6EB",
+        "text-secondary-dark": "B0B3B8",
+        "secondary-dark": "212529",
+      },
+      fontFamily: {
+        alata: ["Alata", "sans"],
+        lora: ["Lora", "sans"],
+      },
+    },
   },
+
   plugins: [],
 };
